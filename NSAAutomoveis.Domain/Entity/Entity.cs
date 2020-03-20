2f38e0284b9ec784e919963a50709ae64b5f08a1
Leonardo Novaes <leonardo.novaes@triasoftware.com.br>
2020-03-20 14:27:48 -0300

    create entity in domains

---
+++ NSAAutomoveis.Domain/Entity/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSAAutomoveis.Domain.Entity
+{
+    public abstract class Entity
+    {
+    }
+}
---
+++ NSAAutomoveis.Domain/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,31 @@
+﻿using NSAAutomoveis.Domain.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NSAAutomoveis.Domain.ObjetoDeValor
+{
+    public class FormaPagamento
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto
+        { 
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartaoCredito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoDeCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
